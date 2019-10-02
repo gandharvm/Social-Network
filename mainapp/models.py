be5a1c63d53642f65501b2db81f52905b1564fe8
harsimar17290 <harsimar.singh@lbbd.in>
2019-10-02 12:03:16 +0530

    Making models - need to fix a bug when a user accepts money request, from_user's amount is not deducted and his transactions are not increased

--- mainapp/models.py
+++ mainapp/models.py
@@ -17,16 +17,21 @@ class User(models.Model):
         to_user=User.objects.get(pk=UserId)
         from_user=self.pk
         to_user.friend_requests.add(from_user)
+        self.save()
+        to_user.save()
     
     def accept_friend_request(self,UserId):
         self.friends.add(UserId)
         self.friend_requests.remove(UserId)
+        self.save()
 
     def reject_friend_request(self,UserId):
         self.friend_requests.remove(UserId)
+        self.save()
 
     def deposit_money(self,amount):
         self.wallet_money+=amount
+        self.save()
     
     def send_money(self,amount,UserId):
         to_user=User.objects.get(pk=UserId)
@@ -34,18 +39,28 @@ class User(models.Model):
         r=MoneyRequest()
         r.amount=amount
         r.from_user=from_user
+        r.save()
         to_user.money_requests.add(r)
+        self.save()
+        to_user.save()
         
 
     def accept_money(self,tid):
-        r=self.money_requests.get(tid)
+        r=MoneyRequest.objects.get(pk=tid)
         self.wallet_money+=r.amount
         u=User.objects.get(pk=r.from_user)
         u.transactions+=1
         u.wallet_money-=r.amount
+        u.save()
+        self.money_requests.remove(tid)
+        self.save()
 
     def reject_money(self,tid):
         self.money_requests.remove(tid)
+        self.save()
+    
+    def __str__(self):
+        return ("%s"%self.pk)
 
 
 
