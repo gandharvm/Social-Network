15c3b6b1c1daddb82d2be0404c709b8e771b6e03
harsimar17290 <harsimar.singh@lbbd.in>
2019-10-02 11:35:40 +0530

    Making models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 class MoneyRequest(models.Model):
-    pass
+    amount=models.FloatField(default=0)
+    from_user=models.IntegerField(default=0)
 
 class User(models.Model):
+
     category=models.CharField(max_length=20)
     friends=models.ManyToManyField("self")
     friend_requests=models.ManyToManyField("self")
     wallet_money=models.FloatField(default=0)
     transactions=models.IntegerField(default=0)
+    money_requests=models.ManyToManyField(MoneyRequest)
 
     def send_friend_request(self,UserId):
         to_user=User.objects.get(pk=UserId)
@@ -26,7 +29,25 @@ class User(models.Model):
         self.wallet_money+=amount
     
     def send_money(self,amount,UserId):
-        pass
+        to_user=User.objects.get(pk=UserId)
+        from_user=self.pk
+        r=MoneyRequest()
+        r.amount=amount
+        r.from_user=from_user
+        to_user.money_requests.add(r)
+        
+
+    def accept_money(self,tid):
+        r=self.money_requests.get(tid)
+        self.wallet_money+=r.amount
+        u=User.objects.get(pk=r.from_user)
+        u.transactions+=1
+        u.wallet_money-=r.amount
+
+    def reject_money(self,tid):
+        self.money_requests.remove(tid)
+
+
 
 class Friendship(models.Model):
     from_friend=models.ForeignKey(User,related_name='friend_set',on_delete=models.CASCADE)
