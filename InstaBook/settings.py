475c9ba6d8bc8fe9bb229ed1d74234112fc4d450
ABHIVANDAN <42318124+abhivandan1999@users.noreply.github.com>
2019-10-17 22:28:44 +0530

    otp mail done in signup
    to add:
    page to show when otp fails

--- InstaBook/settings.py
+++ InstaBook/settings.py
@@ -116,6 +116,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'fcsgroup11project2019@gmail.com'
+EMAIL_HOST_PASSWORD = 'fcs11GUHA'
+EMAIL_PORT = 587
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
