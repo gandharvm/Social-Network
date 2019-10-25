2601c5c8f3c0d48ac88a94eaa3381ee05e562610
harsimar17290 <harsimar.singh@lbbd.in>
2019-10-25 17:46:01 +0530

    s

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from math import inf
 import logging
-import datetime
 
 logger = logging.getLogger('MAINAPP')
 
@@ -11,13 +10,14 @@ class MoneyRequest(models.Model):
     from_user = models.IntegerField(default=0)
 
 
-class User(models.Model):
+class CasualUser(models.Model):
     category = models.CharField(max_length=20)
     money_requests = models.ManyToManyField(MoneyRequest)
     username = models.CharField(max_length=30, unique=True)
+    max_transactions = 15
 
     # private info
-    date_of_birth = models.DateField(default=datetime.date.today)
+    date_of_birth = models.DateField()
     email_id = models.EmailField()
     friends = models.ManyToManyField("self")
     friend_requests = models.ManyToManyField("self")
@@ -40,8 +40,11 @@ class User(models.Model):
         return user
 
     def send_friend_request(self, UserId):
-        to_user = User.objects.get(pk=UserId)
-        from_user = self.pk
+        to_user = CasualUser.objects.filter(pk=UserId)
+        if not to_user.exists():
+            return
+        to_user = to_user[0]
+        from_user = self
         to_user.friend_requests.add(from_user)
         # logger.info('user '+str(self) +
         #             ' sent friend request to '+str(to_user))
@@ -49,16 +52,18 @@ class User(models.Model):
         to_user.save()
 
     def accept_friend_request(self, UserId):
+        fr = self.friend_requests.filter(pk=UserId)
+        if not fr.exists():
+            return
         self.friends.add(UserId)
         self.friend_requests.remove(UserId)
-        # logger.info('user '+str(User.objects.get(pk=UserId)) +
-        #             ' and '+str(self)+' are friends')
         self.save()
 
     def reject_friend_request(self, UserId):
+        fr = self.friend_requests.filter(pk=UserId)
+        if not fr.exists():
+            return
         self.friend_requests.remove(UserId)
-        # logger.info('user '+str(self) + ' rejected friend request from ' +
-        #             str(User.objects.get(pk=UserId)))
         self.save()
 
     def deposit_money(self, amount):
@@ -67,46 +72,58 @@ class User(models.Model):
         self.save()
 
     def send_money(self, amount, UserId):
-        to_user = User.objects.get(pk=UserId)
-        from_user = self.pk
-        r = MoneyRequest(amount=amount, from_user=from_user)
-        r.save()
-        to_user.money_requests.add(r)
-        logger.info(str(self)+' sent money request to ' +
-                    str(to_user)+' for '+str(amount)+' amount')
-        self.save()
-        to_user.save()
+        if(self.transactions < self.max_transactions):
+            to_user = self.friends.filter(pk=UserId)
+            if not to_user.exists():
+                return
+            to_user = to_user[0]
+            from_user = self.pk
+            r = MoneyRequest(amount=amount, from_user=from_user)
+            r.save()
+            to_user.money_requests.add(r)
+            logger.info(str(self)+' sent money request to ' +
+                        str(to_user)+' for '+str(amount)+' amount')
+            self.save()
+            to_user.save()
 
     def accept_money(self, tid):
-        r = MoneyRequest.objects.get(pk=tid)
-        u = User.objects.get(pk=r.from_user)
-        u.transactions += 1
-        u.wallet_money -= r.amount
-        self.wallet_money += r.amount
-        u.save()
-        self.money_requests.remove(tid)
-        logger.info(str(self)+' accepted money request from ' +
-                    str(u)+' for '+str(r.amount)+' amount')
-        self.save()
+        r = self.money_requests.filter(pk=tid)
+        if not r.exists():
+            return
+        r = r[0]
+        u = CasualUser.objects.filter(pk=r.from_user)
+        if not u.exists():
+            self.money_requests.remove(tid)
+            r.delete()
+            self.save()
+        else:
+            u = u[0]
+            if u.transactions < u.max_transactions:
+                u.transactions += 1
+                u.wallet_money -= r.amount
+                self.wallet_money += r.amount
+                u.save()
+                self.money_requests.remove(tid)
+                r.delete()
+                logger.info(str(self)+' accepted money request from ' +
+                            str(u)+' for '+str(r.amount)+' amount')
+                self.save()
 
     def reject_money(self, tid):
+        r = MoneyRequest.objects.filter(pk=tid)
+        if not r.exists():
+            return
+        r = r[0]
         self.money_requests.remove(tid)
-        r = MoneyRequest.objects.get(pk=tid)
-        u = User.objects.get(pk=r.from_user)
-        logger.info(str(self)+' rejected money request from ' +
-                    str(u)+' for '+str(r.amount)+' amount')
+        r.delete()
+        if r.from_user in CasualUser.objects.all():
+            u = u[0]
+            logger.info(str(self)+' rejected money request from ' +
+                        str(u)+' for '+str(r.amount)+' amount')
         self.save()
 
-    def send_message(self, UserId, content):
-        from_user = self
-        to_user = User.objects.get(pk=UserId)
-        msg = Private_Message(from_user=from_user,
-                              to_user=to_user, content=content)
-        msg.save()
-        return msg
-
     def post_on_own_timeline(self, content):
-        post = Post(posted_by=self, content=content, posted_on=self)
+        post = Post(posted_by=self, content=content)
         post.save()
         timeline = Timeline.objects.get(timeline_of=self)
         timeline.posts.add(post)
@@ -114,11 +131,13 @@ class User(models.Model):
         return post
 
     def post_on_other_timeline(self, UserId, content):
-        user = User.objects.get(pk=UserId)
-        post = Post(posted_by=self, content=content, posted_on=user)
-        if(user.others_can_post and self in user.friends.all()):
+        user = self.friends.filter(pk=UserId)
+        if not user.exists():
+            return
+        user = user[0]
+        post = Post(posted_by=self, content=content)
+        if(user.others_can_post):
             post.save()
-            print(post.pk)
             timeline = Timeline.objects.get(timeline_of=user)
             timeline.posts.add(post)
             timeline.save()
@@ -130,20 +149,15 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+    def send_join_request(self, GroupId):
+        group = Group.objects.filter(pk=GroupId)
+        if not group.exists():
+            return
+        group = group[0]
+        if(group.can_send_join_requests):
+            group.join_requests.add(self)
+            group.save()
 
-class Friendship(models.Model):
-    from_friend = models.ForeignKey(
-        User, related_name="friend_set", on_delete=models.CASCADE
-    )
-    to_friend = models.ForeignKey(
-        User, related_name="to_friend_set", on_delete=models.CASCADE
-    )
-
-    class Meta:
-        unique_together = (("to_friend", "from_friend"),)
-
-
-class CasualUser(User):
     def paymentCycleInMonths(self):
         return 1
 
@@ -151,10 +165,10 @@ class CasualUser(User):
         return 0
 
     def maxTransactions(self):
-        return 15
+        return max_transactions
 
     def toPremium(self, plan):
-        fields = [f.name for f in User._meta.fields if f.name != 'id']
+        fields = [f.name for f in CasualUser._meta.fields if f.name != 'id']
         values = dict([(x, getattr(self, x)) for x in fields])
         new_instance = PremiumUser(**values)
         # new_instance.User_ptr = self.User_ptr #re-assign related parent
@@ -164,11 +178,34 @@ class CasualUser(User):
         return(new_instance)
 
 
-class PremiumUser(User):
+class Friendship(models.Model):
+    from_friend = models.ForeignKey(
+        CasualUser, related_name="friend_set", on_delete=models.CASCADE
+    )
+    to_friend = models.ForeignKey(
+        CasualUser, related_name="to_friend_set", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = (("to_friend", "from_friend"),)
+
+
+class PremiumUser(CasualUser):
     plansMap = {'silver': 0, 'gold': 1, 'platinum': 1}
     planCosts = [50, 100, 150]
     plansMaxGroups = [2, 4, inf]
     plan = models.CharField(max_length=10, default="silver")
+    max_transactions = 30
+
+    @classmethod
+    def create(cls, username, dob, email_id, plan):
+        # logger.info("user " + username + " created")
+        user = cls(username=username, date_of_birth=dob,
+                   email_id=email_id, plan=plan)
+        user.save()
+        timeline = Timeline(timeline_of=user)
+        timeline.save()
+        return user
 
     def amountToPay(self):
         return(planCosts[plansMap[self.plan.lower()]])
@@ -177,13 +214,44 @@ class PremiumUser(User):
         return 1
 
     def maxTransactions(self):
-        return 30
+        return max_transactions
 
     def maxGroups(self):
         return(plansMaxGroups[plansMap[self.plan.lower()]])
 
+    def create_group(self, group_name):
+        if(self in GroupAdmin.objects.all()):
+            admin = GroupAdmin.objects.get(user=self)
+        else:
+            admin = GroupAdmin(user=self)
+            admin.save()
+        admin.create_group(username)
 
-class CommercialUser(User):
+    def send_message(self, UserId, content):
+        to_user = self.friends.filter(pk=UserId)
+        if not to_user.exists():
+            return
+        to_user = to_user[0]
+        from_user = self
+        msg = Private_Message(from_user=from_user,
+                              to_user=to_user, content=content)
+        msg.save()
+        return msg
+
+
+class CommercialUser(PremiumUser):
+    max_transactions = inf
+
+    @classmethod
+    def create(cls, username, dob, email_id, plan):
+        # logger.info("user " + username + " created")
+        user = cls(username=username, date_of_birth=dob,
+                   email_id=email_id, plan=plan)
+        user.save()
+        timeline = Timeline(timeline_of=user)
+        timeline.save()
+        return user
+
     def amountToPay(self):
         return 5000
 
@@ -191,15 +259,30 @@ class CommercialUser(User):
         return 12
 
     def maxTransactions(self):
-        return inf
+        return max_transactions
+
+    def create_page(self, content):
+        page = Page(admin, content=content)
+        page.save()
+
+    def send_message(self, UserId, content):
+        to_user = CasualUser.objects.filter(pk=UserId)
+        if not to_user.exists():
+            return
+        to_user = to_user[0]
+        from_user = self
+        msg = Private_Message(from_user=from_user,
+                              to_user=to_user, content=content)
+        msg.save()
+        return msg
 
 
 class Private_Message(models.Model):
     from_user = models.ForeignKey(
-        User, related_name="from_msg", on_delete=models.CASCADE
+        PremiumUser, related_name="from_msg", on_delete=models.CASCADE
     )
     to_user = models.ForeignKey(
-        User, related_name="to_msg", on_delete=models.CASCADE)
+        CasualUser, related_name="to_msg", on_delete=models.CASCADE)
     content = models.CharField(max_length=500)
     time = models.DateTimeField(auto_now_add=True)
 
@@ -219,21 +302,130 @@ class Private_Message(models.Model):
 
 class Post(models.Model):
     posted_by = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posted_by')
+        CasualUser, on_delete=models.CASCADE, related_name='posted_by')
     content = models.CharField(max_length=500)
     time = models.DateTimeField(auto_now=True)
-    posted_on = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posted_on')
 
     def __str__(self):
         return (self.content)
 
 
 class Timeline(models.Model):
-    timeline_of = models.OneToOneField(User, on_delete=models.CASCADE)
-    posts = models.ManyToManyField(Post)
+    timeline_of = models.OneToOneField(CasualUser, on_delete=models.CASCADE)
+    posts = models.ManyToManyField(
+        Post, related_name='timeline')
 
 
 class Page(models.Model):
-    admin = models.ManyToManyField(CommercialUser)
+    admin = models.ForeignKey(CommercialUser, on_delete=models.CASCADE)
     Content = models.CharField(max_length=500)
+
+
+class GroupAdmin(models.Model):
+    user = models.OneToOneField(
+        PremiumUser, on_delete=models.CASCADE, related_name='user')
+    group_count = 0
+
+    def create_group(self, group_name, max_num, can_send):
+        if(max_num < 3):
+            max_num = 20
+        if user.group_count <= user.plansMaxGroups[plansMap[plan]]:
+            group = Group(admin=self, name=group_name,
+                          max_num_of_members=max_num,
+                          can_send_join_requests=can_send)
+            group_count += 1
+            self.save()
+            group.save()
+
+    def delete_group(self, GroupId):
+        group = Group.objects.filter(pk=GroupId)
+        if not group.exists():
+            return
+        group = group[0]
+        if group.admin == self:
+            group.delete()
+            self.group_count -= 1
+            self.save()
+        else:
+            return
+
+    def add_member(self, UserId, GroupId):
+        user = CasualUser.objects.filter(pk=UserId)
+        if not user.exists():
+            return
+        user = user[0]
+        group = Group.objects.filter(pk=GroupId)
+        if not group.exists():
+            return
+        group = group[0]
+        if group.admin == self:
+            group.members.add(user)
+            group.save()
+
+    def remove_member(self, UserId, GroupId):
+        group = Group.objects.filter(pk=GroupId)
+        if not group.exists():
+            return
+        group = group[0]
+        user = group.members.filter(pk=UserId)
+        if not user.exists():
+            return
+        user = user[0]
+        if group.admin == self:
+            group.members.remove(UserId)
+            group.save()
+
+    def accept_join_request(self, GroupId, joinId):
+        group = Group.objects.filter(pk=GroupId)
+        if not group.exists():
+            return
+        group = group[0]
+        user = group.join_requests.filter(pk=joinId)
+        if group.admin == self:
+            if not user.exists() or user not in CasualUser.objects.all():
+                return
+
+            user = user[0]
+            group.members.add(user)
+            group.join_requests.remove(UserId)
+            group.save()
+
+    def reject_join_request(self, GroupId, joinId):
+        group = Group.objects.filter(pk=GroupId)
+        if not group.exists():
+            return
+        group = group[0]
+        user = group.join_requests.filter(pk=joinId)
+        if group.admin == self:
+            if not user.exists():
+                return
+
+            user = user[0]
+            group.join_requests.remove(UserId)
+            group.save()
+
+    def inc_people(self, GroupId, num):
+        group = Group.objects.filter(pk=GroupId)
+        if not group.exists():
+            return
+        group = group[0]
+        if group.admin == self and num > group.max_num_of_members:
+            group.max_num_of_members = num
+
+    def change_join_settings(self, GroupId, setting):
+        group = Group.objects.filter(pk=GroupId)
+        if not group.exists():
+            return
+        group = group[0]
+        if(group.admin == self):
+            group.can_send_join_requests = setting
+
+
+class Group(models.Model):
+    admin = models.ForeignKey(GroupAdmin, on_delete=models.CASCADE)
+    members = models.ManyToManyField(CasualUser, related_name='member_of')
+    name = models.CharField(max_length=20, default='New Group')
+    join_requests = models.ManyToManyField(
+        CasualUser, related_name='sent_join_request_to')
+    max_num_of_members = models.IntegerField(default=20)
+    can_send_join_requests = models.BooleanField(default=False)
