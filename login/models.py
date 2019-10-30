06d1a610bc999074409eedfec4240803e707d429
Gandharv <gandharv17232@iiitd.ac.in>
2019-10-30 21:02:14 +0530

    global

--- login/models.py
+++ login/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TempUser(models.Model):
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=200)
+    email_reg = models.EmailField(max_length=200)
+    category = models.CharField(max_length=50)
+    dob = models.DateField()
+
+
